e404796abfccf10b483d30ff9b7a14ba027fd565
oskar-przybylski <ncsmusic@icloud.com>
2026-04-11 17:40:16 +0200

    Quickfix: Added missing components

---
+++ codium/src/app/shared/ThemeToggle.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { DarkMode, LightMode } from "@mui/icons-material";
+import { IconButton } from "@mui/material";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+
+export function ThemeToggle() {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  const renderIconButton = () => {
+    const switchTheme = () => setTheme(theme === "dark" ? "light" : "dark");
+
+    const icon =
+      theme === "dark" ? (
+        <LightMode fontSize="inherit" />
+      ) : (
+        <DarkMode fontSize="inherit" />
+      );
+
+    return (
+      <IconButton size="large" disableRipple onClick={switchTheme}>
+        {icon}
+      </IconButton>
+    );
+  };
+
+  return <>{renderIconButton()}</>;
+}
